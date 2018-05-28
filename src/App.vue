b5c9e7f4f09e51afb5651fc8a7ddcd5644e8d169
Moryan <972445165@qq.com>
2018-05-28 22:44:41 +0800

    命名视图+嵌套路由+编程式路由+命名路由整合测试

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <router-view name='test' />
+    <!-- <router-view name='title2' /> -->
+    <!-- 默认页面在index.js中可以看到设置为default：Goods -->
+    <!-- title1、2两个页面，在index.js设置好后内容直接
+    在同一页面，也就是index.js中自己设置的'/'这个地址下的页面中显示 -->
+
+    <!-- 根据官网文档还有个叫做# 嵌套命名视图，
+    其实就是嵌套路由和命名视图的结合 -->
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -10,27 +10,27 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/test',
-      name: 'test',
-      component: Test,       //填写路由参数
-      //因为title1,2都是作为test.vue的子路径，所以写在children属性下
-      children: [{
-        path: 'title1',
-        name: 'title1',
-        component: Title1
-      },
-      {
-        path: 'title2',
-        name: 'title2',
-        component: Title2
-      }]
-
-      // 填写children子路由的path不要加/
+      path:'/goods',
+      name:'goods',
+      component:Goods
     },
     {
-      path: '/goods',
-      name: 'goods',
-      component: Goods
+      path: '/',
+      name: '/',
+      components:{
+        default: Goods,
+        test: Test
+      },
+      children: [{
+        path:'test/title1',
+        name:'title1',
+        component:Title1
+      },{
+        path:'test/title2',
+        name:'title2',
+        component:Title2
+      }
+    ]
     }
   ]
 })
