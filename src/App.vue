06ef816849065496afd56b334ef445116de2b24c
Moryan <972445165@qq.com>
2018-05-31 16:57:24 +0800

    头部组件运用与修改

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png">
     <router-view/>
-    <router-view name='test' />
+    <router-view name='test' /> -->
     <!-- <router-view name='title2' /> -->
     <!-- 默认页面在index.js中可以看到设置为default：Goods -->
     <!-- title1、2两个页面，在index.js设置好后内容直接
@@ -10,12 +10,19 @@
 
     <!-- 根据官网文档还有个叫做# 嵌套命名视图，
     其实就是嵌套路由和命名视图的结合 -->
+
+    <my-header></my-header>
+
   </div>
 </template>
 
 <script>
+import MyHeader from '@/components/myHeader';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MyHeader
+  }
 }
 </script>
 
